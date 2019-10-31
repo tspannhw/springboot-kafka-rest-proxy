9350d9acaf0ce98736f4659f929186a5de93518a
Timothy Spann <tspannhw@users.noreply.github.com>
2019-10-31 12:42:31 -0400

    Initial Build

---
+++ src/main/java/dev/datainmotion/kafkarestproxy/KafkaRestProxyApplication.java
@@ -0,0 +1,13 @@
+package dev.datainmotion.kafkarestproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/datainmotion/kafkarestproxy/KafkaRestProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.datainmotion.kafkarestproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRestProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
